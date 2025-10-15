5642735dee6942f6a4754d708e32a2bafb22946d
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-10-14 22:51:42 -0500

    feat(lambda): agregar encabezados CORS a la respuesta de inicio de sesión

--- lambdas/user/login.js
+++ lambdas/user/login.js
@@ -29,6 +29,11 @@ exports.handler = async (event) => {
 
 		return {
 			statusCode: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*", // Permite cualquier origen (desarrollo)
+				"Access-Control-Allow-Methods": "POST, OPTIONS", // Métodos permitidos
+				"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+			},
 			body: JSON.stringify({
 				message: "Login exitoso",
 				tokens: response.AuthenticationResult
